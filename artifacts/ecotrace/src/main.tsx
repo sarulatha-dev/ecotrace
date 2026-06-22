e2b39e5fc45005fac8801cdc7629780ae899dcd5
sarulatha-dev <sarulatha492@gmail.com>
2026-06-22 08:56:14 +0530

    fix vercel build

--- artifacts/ecotrace/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import "./i18n";
-import { createRoot } from "react-dom/client";
-import { setBaseUrl } from "@workspace/api-client-react";
-import App from "./App";
-import "./index.css";
-
-setBaseUrl(import.meta.env.VITE_API_URL || null);
-
-createRoot(document.getElementById("root")!).render(<App />);
